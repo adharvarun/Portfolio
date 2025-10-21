ea893a46d0c83a47d9c6b425b57f1f5cdd0b1f6d
Adharv Arun <adharvarun.10@gmail.com>
2025-10-21 16:13:28 +0400

    achieving perfection

--- src/components/TerminalButton.tsx
+++ src/components/TerminalButton.tsx
@@ -5,7 +5,7 @@ export default function TerminalButton() {
   return (
         <Link
           href="/terminal"
-          className="fixed z-50 bottom-24 right-4 bg-black text-white rounded-full p-4 shadow-lg flex items-center justify-center hover:bg-gray-800 focus:outline-none"
+          className="fixed z-50 bottom-24 right-4 bg-black text-white p-4 rounded-full shadow-lg flex gap-2 hover:scale-105 transition-transform duration-200 hover:shadow-3xl hover:cursor-pointer focus:outline-none"
           aria-label="Terminal"
         >
           <FaTerminal className="w-5 h-5" />
